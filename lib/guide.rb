87c9f7f967ea773e20d641b176450a5a7bb100f6
asotafsiri <aso.tafsiri@gmail.com>
2018-08-20 09:41:56 +0430

    files was modified

---
+++ lib/guide.rb
@@ -0,0 +1,35 @@
+require "restaurant"
+class Guide
+	def initialize(path=nil)
+		#locate the resturant text file at path
+		Restaurant.filepath=path
+		#or create a new file
+		#exit if create fails
+		if Restaurant.file_exists?
+			puts "Found restaurant file. "
+		elsif Restaurant.create_file
+			puts "Created restaurant file. "
+		else
+			puts "Exiting...\n\n"
+			exit!
+		end
+				
+		
+	end
+	def launch!
+		introduction
+		#action loop
+		#wahat do you want to do? (list,find,add,quit)
+		#	do that action
+		#		repeat until user quits
+				conclusion
+	end
+	def introduction
+		puts "\n\n<<< Welcome to the Food Finder >>> \n\n"
+		puts "this is an interactive guide to help you find the food you crave. \n\n"
+	end
+	def conclusion
+		puts "\n<<< Goodby and Bon Appetit ! >>> \n\n\n"
+	end
+
+end
---
+++ lib/restaurant.rb
@@ -0,0 +1,16 @@
+class Restaurant
+	@@filepath=nil
+	def self.filepath=(path=nil)
+		@@filepath=File.join(APP_ROOT,path)
+	end
+	def self.file_exists?
+		#class shoud know if the restaurant exists
+	end
+	def self.create_file
+		#create the restaurant file
+	end
+	def self.saved_restaurants
+		#read the restaurant file
+		#return instances of restaurant
+	end
+end
