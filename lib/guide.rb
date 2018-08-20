dbf112836690d36a2afb309db314e84e3189db46
asotafsiri <aso.tafsiri@gmail.com>
2018-08-20 12:21:05 +0430

    action loop was added to file

--- lib/guide.rb
+++ lib/guide.rb
@@ -19,10 +19,30 @@ class Guide
 	def launch!
 		introduction
 		#action loop
+		result=nil
+		until result== :quit
 		#wahat do you want to do? (list,find,add,quit)
+		print "> "
+		user_response=gets.chomp
 		#	do that action
+		result=do_action(user_response)
 		#		repeat until user quits
-				conclusion
+	    end
+		conclusion
+	end
+	def do_action(action)
+		case action
+		when 'list'
+			puts "Listing....."
+        when 'find'
+			puts "Finding....."
+		when 'add'
+			puts "Adding....."
+		when 'quit'
+			return :quit
+		else 
+			puts "\n I dont underestand that command.\n"
+		end
 	end
 	def introduction
 		puts "\n\n<<< Welcome to the Food Finder >>> \n\n"
